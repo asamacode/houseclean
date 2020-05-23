75d9dda5d329384b787a55e1abdba0830b717f66
asamacode <trhuuluong@gmail.com>
2020-05-23 09:23:37 +0700

    first commit

---
+++ src/main/java/com/asama/houseclean/HousecleanApplication.java
@@ -0,0 +1,13 @@
+package com.asama.houseclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousecleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousecleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asama/houseclean/HousecleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asama.houseclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousecleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
